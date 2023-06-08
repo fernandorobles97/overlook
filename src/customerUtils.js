a7330b46d69dc3896e13e0809d3b8dfddc89091b
Fernando Robles <fernandorobles97@yahoo.com>
2023-06-08 01:05:09 -0500

    feat: Create and pass tests for findBookings function

--- src/customerUtils.js
+++ src/customerUtils.js
@@ -1,5 +1,10 @@
-const findBookings = (user, bookings) => {}
-// this function should grab user.ID and filter bookings to only show bookings that match the userID value
+const findBookings = (user, bookings) => {
+  let filtered = bookings.filter(booking => booking.userID === user.id);
+  if (!filtered.length) {
+    return 'No bookings found.';
+  }
+  return filtered;
+};
 
 export {
   findBookings
--- test/customerTests.js
+++ test/customerTests.js
@@ -1,8 +1,24 @@
 import { expect } from 'chai';
 import { findBookings } from '../src/customerUtils';
+import { bookings1 } from '../test/sampleData.js';
 
 describe('findBookings', () => {
   it('should be a function', () => {
     expect(findBookings).to.be.a('function');
-  })
-})
+  });
+
+  it('should find bookings based on a users ID', () => {
+    const currentUser = {"id": 1, "name": "Leatha Ullrich"};
+    const bookingsForCurrentUser = findBookings(currentUser, bookings1);
+
+    expect(bookingsForCurrentUser.length).to.equal(2);
+    expect(bookingsForCurrentUser[0].userID).to.equal(1)
+  });
+
+  it('should return "No bookings found." if there are no bookings for user', () => {
+    const currentUser1 = {"id": 10101, "name": "Lars Ulrich"};
+    const bookingsForCurrentUser1 = findBookings(currentUser1, bookings1);
+
+    expect(bookingsForCurrentUser1).to.equal('No bookings found.')
+  });
+});
