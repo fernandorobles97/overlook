7363783e26d34a9ead6167a267ee05b277fe5a63
Fernando Robles <fernandorobles97@yahoo.com>
2023-06-08 16:48:24 -0500

    feat: Add query selectors; create functions displayTotalSpent and displayCustomerName: invoke these functions on window load

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,23 +1,37 @@
 //NOTE: Your DOM manipulation will occur in this file
 import { getBookings, getCustomers, getRooms } from './apiCalls';
-let currentCustomer;
+import { findTotalSpent } from './customerUtils';
+var currentCustomer;
 let customersData;
 let roomsData;
 let bookingsData;
 
 // Query Selectors
+const totalSpent = document.querySelector('.total-spent');
+const userGreeting = document.querySelector('.user-greeting');
 
 //Event Listeners
 window.addEventListener('load', () => {
   Promise.all([getBookings(), getCustomers(), getRooms()]).then((data) => {
-    customersData = data[1].customers
-    roomsData = data[2].rooms
-    bookingsData = data[0].bookings
-    setCurrentCustomer()
+    customersData = data[1].customers;
+    roomsData = data[2].rooms;
+    bookingsData = data[0].bookings;
+    setCurrentCustomer();
+    displayTotalSpent();
+    displayCustomerName();
 })
 });
 
 //Event Handlers/Functions
 const setCurrentCustomer = () => {
   return currentCustomer = customersData[Math.floor(Math.random()*customersData.length)];
+};
+
+const displayTotalSpent = () => {
+  let total = findTotalSpent(currentCustomer, bookingsData, roomsData);
+  totalSpent.innerHTML = `Total Spent: $${total.toFixed(2)}`;
+};
+
+const displayCustomerName = () => {
+  userGreeting.innerHTML = `Welcome, ${currentCustomer.name}`;
 };
